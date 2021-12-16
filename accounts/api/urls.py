69c5c79ced94c05e93e15896c7e9c78e39f10600
Gabriel Guerra <gabriel.guerra25@outlook.com>
2021-12-16 04:02:05 +0300

    deleted api from gitignore

---
+++ accounts/api/urls.py
@@ -0,0 +1,28 @@
+from django.urls import path, include
+from accounts.api.views import (
+				
+				account_properties_view,
+				update_account_view,
+				registration_view,
+				ObtainAuthTokenView,
+				does_account_exist_view,
+				ChangePasswordView,
+				LogoutUserView,
+)
+
+
+
+app_name = 'accounts'
+
+urlpatterns = [
+
+	path('check_if_account_exists/', does_account_exist_view, name="check_if_account_exists"),
+	path('change_password/', ChangePasswordView.as_view(), name="change_password"),
+	path('login', ObtainAuthTokenView.as_view(), name="login"),
+	path('properties', account_properties_view, name="properties"),
+	path('properties/update', update_account_view, name="update"),
+	path('register', registration_view, name="register"),
+	path('logouts', LogoutUserView.as_view(), name="logouts"),
+	path('', include('dj_rest_auth.urls')),
+
+]

74e6de87b399b16cb2347835f449fd0614ba65b3
Gabriel Guerra <gabriel.guerra25@outlook.com>
2021-12-22 16:48:00 +0300

    Added logout view from dj_rest_auth library

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -7,7 +7,9 @@ from accounts.api.views import (
 				ObtainAuthTokenView,
 				ChangePasswordView,
 )
-
+from dj_rest_auth.views import (
+    			LogoutView
+)
 
 
 app_name = 'accounts'
@@ -19,6 +21,7 @@ urlpatterns = [
 	path('properties', account_properties_view, name="properties"),
 	path('properties/update', update_account_view, name="update"),
 	path('register', registration_view, name="register"),
-	path('', include('dj_rest_auth.urls')),
+	path('logout/', LogoutView.as_view(), name="logout"),
+	#path('nomy', include('dj_rest_auth.urls')),
 
 ]

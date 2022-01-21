b8b396f50c0a72dee290561368917d76b43b0f87
Gabriel Guerra <gabriel.guerra25@outlook.com>
2022-01-21 16:57:04 +0300

    Added code to sove problem when deleting accounts with outstanding tokens

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from accounts.models import Account
 
-# Register your models here.
+#code added from https://github.com/jazzband/djangorestframework-simplejwt/issues/266#issuecomment-850985081
+#problem: unable to delete accounts with outstanding tokens, lack of permissions
+from rest_framework_simplejwt.token_blacklist import models
+from rest_framework_simplejwt.token_blacklist.admin import OutstandingTokenAdmin
 
+class NewOutstandingTokenAdmin(OutstandingTokenAdmin):
+    def has_delete_permission(self, *args, **kwargs):
+        return True
+admin.site.unregister(models.OutstandingToken)
+admin.site.register(models.OutstandingToken, NewOutstandingTokenAdmin)
+
+
+# Register your models here.
 admin.site.register(Account)
